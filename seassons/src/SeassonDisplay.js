f30f1f9bbc54f73248f64734ea17e4bbafc11873
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-16 18:27:53 -0300

    Determine seasson

--- seassons/src/SeassonDisplay.js
+++ seassons/src/SeassonDisplay.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
-const SeassonDisplay = () => {
+const getSeasson = (lat, month) => {
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    }else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeassonDisplay = props => {
+    const seasson = getSeasson(props.lat, new Date().getMonth());
+
+    console.log(seasson);
     return(
         <div>Seasson Display!</div>
     );
