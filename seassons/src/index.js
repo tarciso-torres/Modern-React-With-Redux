948ef2f9f21217fadf52fce9f987edd8c764ddad
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-16 08:25:51 -0300

    Conditionally rendering content

--- seassons/src/index.js
+++ seassons/src/index.js
@@ -20,13 +20,17 @@ class App extends React.Component {
         );
     }
 
+    // React says we have to define render!!
     render(){
-        return( 
-        <div>
-            Location: { this.state.lat }
-            <br />
-            Error: { this.state.errorMessage }
-        </div>);
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: { this.state.errorMessage } </div>
+        }
+
+        if(this.state.lat && !this.state.errorMessage){
+            return <div>Location: { this.state.lat }</div>
+        }
+        
+        return <div>Loading!</div>
     }
 }
 
