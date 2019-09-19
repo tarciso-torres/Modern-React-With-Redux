1b38925874ffafefd0959781568a67f8b7bd2bf4
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-19 16:55:21 -0300

    Invoking callbacks in children

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import SeachBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SeachBar />            
-        </div>
-    );
-};
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+    render(){
+        return (
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SeachBar onSubmit={this.onSearchSubmit}/>            
+            </div>
+        );
+    }
+}
 
 export default App;
