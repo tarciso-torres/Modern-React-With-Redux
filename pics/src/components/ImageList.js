d729f6816bbf4782b6e65e697c71e80b2968cd42
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-27 12:13:02 -0300

    Grid CSS

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,11 @@
+import './ImageList.css';
 import React from 'react';
 
 const ImageList = (props) => {
     const images = props.images.map(( {description, id, urls} ) => {
         return <img alt={description} key={id} src={urls.regular} />
     })
-    return <div>{images}</div>
+    return <div className="image-list">{images}</div>
 }
 
 export default ImageList;
