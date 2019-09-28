cd131cd7c18d8e5dd869b47d4dee1d951cbf1fab
Tarciso Torres <tarcisoftorres@gmail.com>
2019-09-28 19:22:17 -0300

    Dynamic spans

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID '
+        Authorization: 'Client-ID 572e5261a1261effbb8ff7bf226f70147ce191484eb1846a3731526fba531b67'
     }
 })
