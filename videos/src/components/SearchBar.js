510abd17ff24edd82ce87aabc2c0edf730aedbee
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-06 08:17:51 -0300

    Reminder on Event Handlers

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: ''};
+
+    onInputChange = () => {
+
+    }
+    
     render() {
         return(
-            <div>SearchBar</div>
+            <div className = 'search-bar ui segment'>
+                <form className = 'ui form'>
+                    <div className = 'field'>
+                        <label>Video search</label>
+                        <input 
+                            type='text' 
+                            value={this.state.term}
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
+            </div>
         )
     }
 }
