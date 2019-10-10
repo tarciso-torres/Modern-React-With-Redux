cfa314f8a6ab8eaf5a273e9598ca1de4856f5894
Tarciso Torres <tarcisoftorres@gmail.com>
2019-10-10 16:29:12 -0300

    Styling the VideoDetail

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
+    
     if(!video) {
         return (
             <div>Loading...</div>
         );
     }
-    
+
+    const snippet = video.snippet;
     return(
-        <div>{video.snippet.title}</div>
+        <div className='ui segment'>
+            <h4 className='ui header'>{snippet.title}</h4>
+            <p>{snippet.description}</p>
+        </div>
+        
     );
 }
 
